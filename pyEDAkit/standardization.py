1746aafb4a65ba2f9629929990fbc440c1c628ad
Marco Schivo <marcoschivo1@gmail.com>
2024-09-30 12:14:00 +0200

    add standardization with standard deviation, and test with iris dataset

--- pyEDAkit/standardization.py
+++ pyEDAkit/standardization.py
@@ -1,9 +1,19 @@
 import numpy as np
 
-def
-
-def with_std(data : np.ndarray, zero_mean=True):
-    pass
+def with_std_dev(data : np.ndarray, zero_mean=True):
+    stds_data  = np.std(data, axis=0)
+    means_data = np.mean(data, axis=0)
+    if zero_mean:
+        #  z_score variables will have:
+        #  mean = 0
+        #  variance = 1
+        z_scores = (data - means_data) / stds_data
+    else:
+        #  z_score variables will have:
+        #  mean = mean / std
+        #  variance = 1
+        z_scores = data / stds_data
+    return z_scores
 
 def with_range():
     pass
--- test.py
+++ test.py
@@ -1,6 +1,33 @@
-from pyEDAkit import standardization as std
+from numpy.core.fromnumeric import size
+from pyEDAkit import standardization as eda_std
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
-iris_df = pd.read_csv("data/iris.data")
+def scatter(x, y, title='Title'):
+    plt.figure()
+    color = np.arange(len(x))
+    color.fill(np.random.rand())
+    plt.scatter(x, y, c=color, label='data point', s=3)
+    plt.axhline(0, color='gray', linestyle='--')  # Horizontal dotted line at y=0
+    plt.axvline(0, color='gray', linestyle='--')  # Vertical dotted line at x=0
+    # plt.xlabel('Index')
+    # plt.ylabel('Value')
+    plt.title(title)
+    plt.legend()
+    plt.draw()
 
-iris_df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+df = pd.read_csv("data/iris/iris.data")
+df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+
+z_scores_zero = eda_std.with_std_dev(df[['sepal_length', 'petal_length']].to_numpy(), zero_mean=True)
+z_scores_not_zero = eda_std.with_std_dev(df[['sepal_length', 'petal_length']].to_numpy(), zero_mean=False)
+
+scatter(df['sepal_length'], df['petal_length'], title='Original data')
+scatter(z_scores_zero[:, 0], z_scores_zero[:, 1], title='z_scores with mean 0')
+scatter(z_scores_not_zero[:, 0], z_scores_not_zero[:, 1], title='z_scores with NOT mean 0')
+
+print(np.std(z_scores_zero, axis=0), np.mean(z_scores_zero, axis=0))
+print(np.std(z_scores_not_zero, axis=0), np.mean(z_scores_not_zero, axis=0))
+
+plt.show()
