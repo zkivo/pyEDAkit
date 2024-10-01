a286e2e8f075d16c2e1fa556e3cca86e0a8be7d5
Marco Schivo <marcoschivo1@gmail.com>
2024-10-01 12:06:28 +0200

    add standardization with min-max normalization

--- pyEDAkit/standardization.py
+++ pyEDAkit/standardization.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def with_std_dev(data : np.ndarray, zero_mean=True):
+    z_scores = None
     stds_data  = np.std(data, axis=0)
     means_data = np.mean(data, axis=0)
     if zero_mean:
@@ -15,8 +16,19 @@ def with_std_dev(data : np.ndarray, zero_mean=True):
         z_scores = data / stds_data
     return z_scores
 
-def with_range():
-    pass
+def with_range(data : np.ndarray, bounded=True):
+    # Also called min-max normalization,
+    # it restrains the data into the range between 1 and 0
+    # With the bounded variable set as True, the data are also
+    # at each end point.
+    z  = None
+    min = np.min(data, axis=0)
+    max = np.max(data, axis=0)
+    if bounded:
+        z = (data - min) / (max - min)
+    else:
+        z = data / (max - min)
+    return z
 
 def with_sphering():
     pass
