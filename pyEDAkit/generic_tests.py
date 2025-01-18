a6d803defc516e26b3db18222b290a7d683d9baa
alber <alberto.biscalchin@gmail.com>
2025-01-18 13:37:53 +0100

    Update img link

---
+++ pyEDAkit/generic_tests.py
@@ -0,0 +1,15 @@
+from pyEDAkit import *
+
+##############################################
+############ Generic Tests ###################
+##############################################
+"""
+Questo documento python è stato creato per testare la versione caricata su PyPi
+del modulo pyEDAkit installato da pip con il comando:
+
+pip install pyEDAkit
+
+questo file è un file spazzatura che può essere usato a piacimento libero da ogni dipendenza
+
+"""
+
